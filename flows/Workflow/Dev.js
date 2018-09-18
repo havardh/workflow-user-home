905ffc8399a24eb5a918108d6bc491c8adb36485
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-09-18 08:18:18 +0200

    fix: increase terminal size for Workflow/Dev

--- flows/Workflow/Dev.js
+++ flows/Workflow/Dev.js
@@ -10,8 +10,8 @@ const Emacs = requireComponent("workflow-app-emacs");
 export default render(
   <Workspace name={"workflow"}>
     <SplitV percent={1.0}>
-      <Emacs percent={0.9} file={"~/dev/workflow"} />
-      <Terminal percent={0.1} cwd={"~/dev/workflow"} />
+      <Emacs percent={0.8} file={"~/dev/workflow"} />
+      <Terminal percent={0.2} cwd={"~/dev/workflow"} />
     </SplitV>
   </Workspace>
 );
